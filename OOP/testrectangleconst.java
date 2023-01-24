a866f3d36b6672fd5b280969b18d23bd6d3c5a6b
Darshanjain209 <djain4252@gmail.com>
2023-01-25 03:32:02 +0530

    darshan

---
+++ OOP/testrectangleconst.java
@@ -0,0 +1,14 @@
+package in.co.OOP;
+
+public class testrectangleconst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		rectangleconst rc=new rectangleconst(12,18);
+		System.out.println("length="+rc.getlength());
+		System.out.println("width="+rc.getwidth());
+		
+
+	}
+
+}
